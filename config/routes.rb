5085350132051205f079299468fc0c4f63a9d15c
Chau Rails <creationsbychau@gmail.com>
2018-07-12 18:12:31 -0400

    Create rails pages and about and home

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/home', to: 'pages#home'
+  get 'pages/about', to: 'pages#about'
+
+  resources :categories, except: [:destroy]
+
+  resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+end
